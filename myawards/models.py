fbf5a205b9d5fd247347271c8e2a0f2d04954beb
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-10 11:35:20 +0300

    Created a project model

--- myawards/models.py
+++ myawards/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.urls import reverse
+
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=255)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    developer = models.ForeignKey(User, on_delete=models.CASCADE)
+    technologies = models.CharField(max_length=200, blank=True)
+    snapshot = ImageField(manual_crop='1280x720')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- myawards/views.py
+++ myawards/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.http import HttpResponse
+# from .models import Project
+
 
 # Create your views here.
