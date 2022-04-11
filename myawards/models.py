8484589812e1ec7464d7f72b2692f415ac61a5f1
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-11 19:23:08 +0300

    Added a review model class

--- myawards/models.py
+++ myawards/models.py
@@ -47,23 +47,24 @@ class Project(models.Model):
     
     
     class Rating(models.Model):
-        design = models.IntegerField(choices=rating, default=0, blank=True)
-        usability = models.IntegerField(choices=rating, blank=True)
-        content = models.IntegerField(choices=rating, blank=True)
+        design = models.IntegerField(blank=True, null=True, default=0 )
+        usability = models.IntegerField(blank=True, null=True, default=0)
+        content = models.IntegerField(blank=True, null=True, default=0)
         score = models.FloatField(default=0, blank=True)
-        design_average = models.FloatField(default=0, blank=True)
-        usability_average = models.FloatField(default=0, blank=True)
-        content_average = models.FloatField(default=0, blank=True)
-        user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
-        post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
+        project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings', null=True)
 
         def save_rating(self):
             self.save()
 
-        # @classmethod
-        # def get_ratings(cls, id):
-        #     ratings = Rating.objects.filter(post_id=id).all()
-        #     return ratings
-
         def __str__(self):
             return f'{self.post} Rating'
+        
+        
+class Reviews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviewer')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    review = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.review
