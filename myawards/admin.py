3a2171981fdf57fe87ccff68a5cfd3c93b786096
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-11 19:25:22 +0300

    Registered Models

--- myawards/admin.py
+++ myawards/admin.py
@@ -4,3 +4,5 @@ from .models import Profile, Project, Rating ,Reviews
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(Rating)
+admin.site.register(Reviews)
